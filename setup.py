b601f8f13dc94a8627feaef9d1a47fb1b214a31c
Lasse Regin Nielsen <lasseregin@gmail.com>
2020-12-02 17:26:46 +0100

    Create setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="firestore-collections",
+    version="0.0.1",
+    author="Lasse Regin Nielsen",
+    author_email="lasseregin@gmail.com",
+    description="Simple Firestore collection definitions and queries using pydantic schemas and Firestore query API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
