a23bbe2146819d28c9b7a51a266cbe013ccf3378
Lasse Regin Nielsen <lasseregin@gmail.com>
2021-02-17 14:39:16 +0100

    Import SchemaWithOwner in root module

--- firestore_collections/__init__.py
+++ firestore_collections/__init__.py
@@ -1,10 +1,14 @@
 from firestore_collections.collection import Collection
 from firestore_collections.enums import OrderByDirection
-from firestore_collections.schema import Schema
+from firestore_collections.schema import (
+    Schema,
+    SchemaWithOwner,
+)
 
 
 __all__ = [
     'Collection',
     'OrderByDirection',
     'Schema',
+    'SchemaWithOwner',
 ]
